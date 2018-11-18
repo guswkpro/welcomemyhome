d5fc241209f79b5dca9be3dd0bf2c18e7aa05120
guswkpro <guswk_pro@naver.com>
2018-11-18 20:08:00 +0900

    Update angularUser.js

--- views/js/angularUser.js
+++ views/js/angularUser.js
@@ -10,20 +10,26 @@ app.controller('signupController', function ($scope, $http) {
 	console.log('aaa');
 	$scope.pushData = function () {
 		console.log('aaaa');
-		$http({
-			method: 'POST',
-			url: '/test',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			data: ({
-				'id': 'sw1',
-				'pw': '1234',
-				'nickname': 'mongo1'
-			})
-		}).success(function (response) {
+		// $http({
+		// 	method: 'POST',
+		// 	url: '/test',
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+		// 	data: ({
+		// 		'id': 'sw1',
+		// 		'pw': '1234',
+		// 		'nickname': 'mongo1'
+		// 	})
+		// }).success(function (response) {
+		// 	console.log(response);
+		// }).finally(function () {
+		// 	console.log('complete');
+		// });
+
+		$http.post('/test', {'test' : 'test'}).success(function (response){
 			console.log(response);
-		}).finally(function () {
+		}).finally(function(){
 			console.log('complete');
 		});
 	}
