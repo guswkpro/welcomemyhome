90a4f5a2dbd1e74acdee2f6bbe6e79208422bc3b
guswkpro <guswk_pro@naver.com>
2018-11-18 22:36:21 +0900

    Update index.js

--- dao/userdao/index.js
+++ dao/userdao/index.js
@@ -32,7 +32,7 @@ exports.signup = function (user, callback) {
 ********************/
 exports.edituserconnectdate = function (user_idx, date, callback) {
     client.query('UPDATE stweb.stweb_users set user_recent_date = ? where user_idx = ?', [date, user_idx], function (error, result, fields) {
-        console.log(error);
+        console.log(date + 'aasbsad');
         console.log(result);
         callback(error);
     });
