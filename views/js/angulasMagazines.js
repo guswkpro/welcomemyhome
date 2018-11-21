c2063cf5a830b74d1b687f0836120418ef8a34b4
nocucumberno <nocucumberno@users.noreply.github.com>
2018-11-21 21:29:12 +0900

    하트

--- views/js/angulasMagazines.js
+++ views/js/angulasMagazines.js
@@ -1,6 +1,14 @@
 $(function(){
 	$(document).one('click', '.like-review', function(e) {
-		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> 좋아요');
+		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> 좋아요♡');
 		$(this).children('.fa-heart').addClass('animate-like');
 	});
 });
+
+
+$(function() {
+
+  $(".heart").on("click", function() {
+    $(this).toggleClass("heart-blast");
+  });
+}); 
