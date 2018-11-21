08b6e9e4527ee3b259ddd5d678429ef6503db05c
nocucumberno <nocucumberno@users.noreply.github.com>
2018-11-21 20:52:52 +0900

    ㅈㅇㅈㅇㅈㅇ

--- views/js/angulasMagazines.js
+++ views/js/angulasMagazines.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(document).one('click', '.like-review', function(e) {
-		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked this');
+		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> 좋아요');
 		$(this).children('.fa-heart').addClass('animate-like');
 	});
 });
