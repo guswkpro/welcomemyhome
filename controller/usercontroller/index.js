0b84014ab14cab08bd9bb9ec13291bbd1c1bc94b
guswkpro <guswk_pro@naver.com>
2018-11-21 22:37:12 +0900

    Update index.js

--- controller/usercontroller/index.js
+++ controller/usercontroller/index.js
@@ -10,7 +10,7 @@ require('date-utils');
         GET
 ********************/
 exports.logincheck = function (request, response) {
-	console.log(request.header);
+	console.log(request.header.cookie);
 	console.log(request.session);
 };
 
