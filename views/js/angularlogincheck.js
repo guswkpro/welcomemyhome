5035e4207922fbb015039f96012453d877e93969
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-23 04:25:02 +0900

    update

--- views/js/angularlogincheck.js
+++ views/js/angularlogincheck.js
@@ -5,15 +5,15 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
     $http.get('/logincheck').success(function(response) {
       console.log(response.RESULT);
       if (response.RESULT == "1") {
+        $scope.div_login = {
+          "width" :  "20%"
+        };
         $scope.showHide_login = true;
       } else if (response.RESULT == "0") {
         var msg = "알수없는 오류로 로그인이 끊겼습니다.";
         $window.alert(msg);
         $window.location.href = '/';
       } else {
-        $scope.div_login = {
-          "width" :  "20%"
-        };
         $scope.showHide_logout = true;
       }
     });
