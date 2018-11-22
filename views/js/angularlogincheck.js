00383bccceddc7c2ccbe1eaea67fbfdae8a0e8fc
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-23 02:24:23 +0900

    update nav

--- views/js/angularlogincheck.js
+++ views/js/angularlogincheck.js
@@ -5,9 +5,13 @@ angular.element(document).ready(function() {
     $http.get('/logincheck').success(function(response) {
       console.log(response.RESULT);
       if (response.RESULT == "1") {
-
+          $scope.showHide_login = true;
+          $scope.showHide_logout = false;
       } else {
-
+        var msg = "알수없는 오류로 로그인이 끊겼습니다.";
+				$window.alert(msg);
+        $scope.showHide_login = false;
+        $scope.showHide_logout = true;
       }
     });
   });
