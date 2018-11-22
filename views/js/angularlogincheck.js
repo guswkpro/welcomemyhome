083e55916f171547dced84ec5acbdc81e8ae400b
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-23 00:44:15 +0900

    update

--- views/js/angularlogincheck.js
+++ views/js/angularlogincheck.js
@@ -2,22 +2,13 @@ var myApp = angular.module('myApp', []);
 
 angular.element(document).ready(function() {
   myApp.controller('logincheckCtrl', function($scope, $http) {
-    $http({
-      method: 'GET',
-      url: '/',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      },
-      params: {
-        user_auth: user_auth
-      }
-    }).success(function(response) {
+    $http.get('/logincheck').success(function(response) {
       console.log(response.RESULT);
       if (response.RESULT == "1") {
 
       } else {
 
       }
-    })
+    });
   });
-})
+});
