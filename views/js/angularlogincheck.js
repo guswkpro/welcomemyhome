40c1133a22d94e9d1247d7498586ea4f7a161b4a
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-23 02:51:52 +0900

    Update angularlogincheck.js

--- views/js/angularlogincheck.js
+++ views/js/angularlogincheck.js
@@ -1,15 +1,14 @@
 var myApp = angular.module('myApp', []);
 
-  myApp.controller('logincheckCtrl', function($scope, $http, $window) {
-    $http.get('/logincheck').success(function(response) {
-      if (response.RESULT == "1") {
-        $scope.showHide_login = !$scope.showHide_login;
-        $scope.showHide_logout = !$scope.showHide_logout;
-      } else {
-        var msg = "알수없는 오류로 로그인이 끊겼습니다.";
-        $window.alert(msg);
-        $scope.showHide_login = false;
-        $scope.showHide_logout = true;
-      }
-    });
+myApp.controller('logincheckCtrl', function($scope, $http, $window) {
+  $http.get('/logincheck').success(function(response) {
+    console.log(response.RESULT);
+    if (response.RESULT == "1") {
+      $scope.showHide_logout = false;
+    } else {
+      var msg = "알수없는 오류로 로그인이 끊겼습니다.";
+      $window.alert(msg);
+      $scope.showHide_login = false;
+    }
   });
+});
