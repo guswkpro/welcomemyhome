05d3594d68a80ce283e255b6c682a57ff03ce3e2
guswkpro <guswk_pro@naver.com>
2018-11-23 02:14:51 +0900

    Update index.js

--- dto/estimatedto/index.js
+++ dto/estimatedto/index.js
@@ -9,7 +9,7 @@ exports.estimate = function(user_idx, content, date, imagepath, callback){
 
 exports.estimateanswer = function (user_idx, estimate_idx, content, date, imagepath, callback) {
     var answer = {};
-    amswer.user_idx = user_idx;
+    answer.user_idx = user_idx;
     answer.estimate_idx = estimate_idx;
     answer.answer_content = content;
     answer.answer_post_date = date;
