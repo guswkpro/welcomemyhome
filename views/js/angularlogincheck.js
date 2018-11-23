294ac6a45e1ac3e837b3b1845ce6e73276ec9ced
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-23 10:29:49 +0900

    update estimate

--- views/js/angularlogincheck.js
+++ views/js/angularlogincheck.js
@@ -1,5 +1,6 @@
 var app = angular.module('Main', []);
 
+// 화면 전환 시 login check 기능
 app.controller('logincheckCtrl', function($scope, $http, $window) {
   $scope.load = function() {
     $http.get('/logincheck').success(function(response) {
@@ -17,5 +18,41 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
         $scope.showHide_logout = true;
       }
     });
+  };
+});
+
+// estimate 제출 시 정보 서버로 보내는 기능
+app.controller('estimateCtrl', function($scope, $http, $window) {
+  $scope.pushData = function () {
+    $http({
+      method: 'POST',
+      url: '/addestimate',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: ({
+        title: $scope.title,
+        content: $scope.content,
+        address: $scope.address,
+      })
+    }).success(function (response) {
+      if (response.RESULT == "1") {
+        var msg = "견적 작성에 성공하셨습니다.";
+        $window.alert(msg);
+        $window.location.href = '/getestimatelist';
+      } else {
+        var msg = "알 수 없는 오류로 견적 작성에 실패하였습니다.";
+        $window.alert(msg);
+      }
+    });
+  };
+});
+
+// Estimate 작성 취소
+app.controller('estimateCtrl', function($scope, $window) {
+  $scope.cancelEstimate = function () {
+    var msg = "작성을 취소하여 리스트 페이지로 이동합니다.";
+    $window.alert(msg);
+    $window.location.href = '/getmagazinelist';
   };
 });
