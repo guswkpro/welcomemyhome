b745f05f5366af71da8a0c4c4bf49aefb08364ff
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-24 04:45:22 +0900

    update

--- views/js/angularMain.js
+++ views/js/angularMain.js
@@ -2,16 +2,13 @@ var app = angular.module('Main', []);
 
 // 화면 전환 시 login check 기능
 app.controller('logincheckCtrl', function($scope, $http, $window) {
-  var auth;
   $scope.load = function() {
     $http.get('/logincheck').success(function(response) {
       console.log(response.RESULT);
       if (response.RESULT == "1") {
-        console.log(response.auth);
-        auth = response.auth;
-        console.log("aaaa"+auth);
+        $scope.auth = response.auth;
         $scope.div_login = {
-          "width" :  "12%"
+          "width": "12%"
         };
         $scope.showHide_login = true;
       } else if (response.RESULT == "0") {
@@ -24,7 +21,7 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
     });
   };
   $scope.checkauth = function() {
-    if(auth == "1"){
+    if (scope.auth == "1") {
 
     }
   };
@@ -32,7 +29,7 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
 
 // estimate 제출 시 정보 서버로 보내는 기능
 app.controller('estimateCtrl', function($scope, $http, $window) {
-  $scope.pushEstimateData = function () {
+  $scope.pushEstimateData = function() {
     $http({
       method: 'POST',
       url: '/addestimate',
@@ -43,9 +40,9 @@ app.controller('estimateCtrl', function($scope, $http, $window) {
         title: $scope.title,
         content: $scope.content,
         address: $scope.address,
-        picture : $scope.picture
+        picture: $scope.picture
       })
-    }).success(function (response) {
+    }).success(function(response) {
       if (response.RESULT == "1") {
         console.log(response.RESULT);
         var msg = "견적 작성에 성공하셨습니다.";
@@ -61,44 +58,46 @@ app.controller('estimateCtrl', function($scope, $http, $window) {
 
 // Estimate 작성 취소
 app.controller('estimateCtrl', function($scope, $window) {
-  $scope.cancelEstimate = function () {
+  $scope.cancelEstimate = function() {
     var msg = "작성을 취소하여 리스트 페이지로 이동합니다.";
     $window.alert(msg);
     $window.location.href = '/estimatelist';
   };
 });
 
-//Estimate 리스트 pagenation
-app.controller('estimateListCtrl', function($scope, $http, $routeParams, $window){
-  $scope.currentPage=0;
-  $scope.pageSize = 10;
-  $scope.numberOfPages=function() {
-    return Math.ceil($scope.data.length/$scope.pageSize);
-  };
-  $scope.checkcurrentPage=function() {
-    if($scope.currentPage == 0){
-      $scope.checkzero = true;
-    }
-    else{
-      $scope.checkzero = false;
-      $scope.currentPage = $scope.currentPage-1;
-    }
-  };
+//
+app.controller('estimateListCtrl', function($scope, $http, $routeParams, $window) {
   $scope.load = function() {
     $http.get('/getestimatelist', {
-      params : {
-        user_idx : $routeParams.id,
-        offset : $routeParams.offset
+      params: {
+        user_idx: $routeParams.id,
+        offset: $routeParams.offset
       }
     }).success(function(response) {
-      if(response.RESULT == "1") {
+      if (response.RESULT == "1") {
         $scope.data = response.INFO;
-      }
-      else {
+      } else {
         var msg = "알 수 없는 오류로 리스트를 불러올 수 없습니다.";
         $window.alert(msg);
         $window.location.href = '/estimate';
       }
     });
+  };
+});
+
+//Estimate 리스트 pagenation
+app.controller('estimatePageNationCtrl', function($scope, $http, $routeParams, $window) {
+  console.log("aaaaaa");
+  $scope.currentPage = 0;
+  $scope.pageSize = 10;
+  $scope.numberOfPages = function() {
+    return Math.ceil($scope.data.length / $scope.pageSize);
+  };
+  $scope.checkcurrentPage = function() {
+    if ($scope.currentPage == 0) {
+      $scope.checkzero = true;
+    } else {
+      $scope.checkzero = false;
+    }
   };
 });
