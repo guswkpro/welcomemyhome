8a86203a7e98b386c13ca8213c2db40d82b9ec95
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-25 04:09:17 +0900

    Update index.js

--- controller/estimatecontroller/index.js
+++ controller/estimatecontroller/index.js
@@ -9,7 +9,7 @@ require('date-utils');
         GET
 ********************/
 exports.getestimatelist = function (request, response) {
-    var req_user_check = request.params("user_idx");
+    var req_user_check = request.param("user_idx");
     var req_offset = request.param('offset');
     var info = [];
     async.waterfall([
