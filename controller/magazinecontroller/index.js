9d95d07ec441fe0d1da19b045692981c7f4d7e09
nocucumberno <nocucumberno@users.noreply.github.com>
2018-11-25 21:17:31 +0900

    Update index.js

--- controller/magazinecontroller/index.js
+++ controller/magazinecontroller/index.js
@@ -94,6 +94,7 @@ exports.getmagazinedetail = function (request, response) {
         }, function (magazinedata, nextCallback) {
             var encodedimage = [];
             var count = 0;
+            console.log(magazinedata);
             magazinedata[0].magazine_picture_path = magazinedata[0].magazine_picture_path.split(',');
             async.whilst(function () {
                 return count < (magazinedata[0].magazine_picture_path.length);
