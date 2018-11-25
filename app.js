4f17fbde3492570ba61bea82c0aca418a1564767
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-26 05:54:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(session({
         saveUninitialized: true
 }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/views'));
 
