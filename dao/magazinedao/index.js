2981636b537a7f713bbfcef13c064a516fb6ae1b
nocucumberno <nocucumberno@users.noreply.github.com>
2018-11-25 21:19:45 +0900

    Update index.js

--- dao/magazinedao/index.js
+++ dao/magazinedao/index.js
@@ -18,6 +18,8 @@ exports.getmagazinelist = function (offset, callback) {
 };
 exports.getmagazinedetail = function (magazine_idx, callback) {
     client.query('SELECT * FROM stweb.stweb_magazines where magazine_idx = ?', [magazine_idx], function (error, result) {
+      console.log(result);
+      console.log(magazine_idx);
         callback(error, result);
     });
 };
