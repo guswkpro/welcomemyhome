838a46a3320b6cd1a109aeb07545eef00808be38
nocucumberno <nocucumberno@users.noreply.github.com>
2018-11-25 21:09:40 +0900

    update

--- views/js/angularMagazinedetail.js
+++ views/js/angularMagazinedetail.js
@@ -1,6 +1,55 @@
 var app = angular.module('magazinedetail', []);
 
-$http.get('/getestimatelist', {
-  params: {
-    magazine_idx: 15
-  }
+//우진이 네비바 로그인 체크
+app.controller('logincheckCtrl', function($scope, $http, $window) {
+  $scope.load = function() {
+    $http.get('/logincheck').success(function(response) {
+      console.log(response.RESULT);
+      if (response.RESULT == "1") {
+        $scope.div_login = {
+          "width": "12%"
+        };
+        $scope.showHide_login = true;
+      } else if (response.RESULT == "0") {
+        var msg = "알수없는 오류로 로그인이 끊겼습니다.";
+        $window.alert(msg);
+        $window.location.href = '/';
+      } else {
+        $scope.showHide_logout = true;
+      }
+    });
+  };
+});
+
+
+app.controller('magazinetitle', function($scope, $http) {
+  $scope.load = function() {
+    $http.get('/getmagazinedetail', {
+      params: {
+        offset: offset
+      }
+    })
+  };
+});
+
+
+  $http.get('/getestimateanswerlist', {
+    params: {
+      offset: offset
+    }
+   }).success(function(response) {
+          if (response.RESULT == 1) {
+            data_my = response.INFO;
+            console.log(data_my, "user1");
+            total_my = 15; // response.total_my
+            total = total_my;
+          } else {
+            var msg = "알 수 없는 에러로 리스트를 불러 올 수 없습니다.";
+            $window.alert(msg);
+            $window.location.href = '/';
+          }
+        else { //로그인 안 했을 시
+        var msg = "로그인을 해주세요";
+        $window.alert(msg);
+        $window.location.href = '/';
+      });
--- views/js/magazinedetail.js
+++ views/js/magazinedetail.js
@@ -6,39 +6,3 @@ $(function() {
     $(this).toggleClass("heart-blast");
   });
 });
-
-
-//사진슬라이드쇼
-$(document).ready(function(){
-  var imgs;
-  var img_count;
-  var img_position = 1;
-
-  imgs = $(".slide ul");
-  img_count = imgs.children().length;
-
-  //버튼클릭시 함수실행
-  $('#back').click(function(){
-    back();
-  });
-  $('#next').click(function(){
-    next();
-  });
-
-  function back() {
-    if(1<img_position){
-      imgs.animate({
-        left:'-=1000px'
-      });
-      imgs_position--;
-    }
-  }
-  function next() {
-    if(1<img_position){
-      imgs.animate({
-        left:'+=1000px'
-      });
-      imgs_position++;
-    }
-  }
-});
