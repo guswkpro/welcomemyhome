6e1e75a53fbeff35e7bfafb8d3875c912327922c
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-25 20:59:08 +0900

    Update angularMain.js

--- views/js/angularMain.js
+++ views/js/angularMain.js
@@ -24,8 +24,8 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
 // estimate 제출 시 정보 서버로 보내는 기능
 app.controller('estimateCtrl', function($scope, $http, $window) {
   $scope.pushEstimateData = function() {
-    var encodedimage = $window.btoa(unescape(encodeURIComponent($scope.image)));
-    $scope.image = JSON.stringify(encodedimage);
+    $scope.image = $window.btoa(unescape(encodeURIComponent($scope.image)));
+    //$scope.image = JSON.stringify(encodedimage);
     console.log($scope.image, "stringfy");
     $http({
       method: 'POST',
