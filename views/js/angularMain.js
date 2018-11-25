82fdaab21ed08653bff5ec1da22cfe633a16ec0d
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-25 22:50:46 +0900

    update

--- views/js/angularMain.js
+++ views/js/angularMain.js
@@ -24,6 +24,10 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
 // estimate 제출 시 정보 서버로 보내는 기능
 app.controller('estimateCtrl', function($scope, $http, $window) {
   $scope.pushEstimateData = function() {
+    function getRealPath(obj){
+      obj.select();
+      document.upimage.real_path.value=document.selection.createRangeCollection()[0].text.toString();
+    }
     console.log($scope.image, "image");
     $scope.image = $window.btoa(unescape(encodeURIComponent($scope.image)));
     //$scope.image = JSON.stringify(encodedimage);
