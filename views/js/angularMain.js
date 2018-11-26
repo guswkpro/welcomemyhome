01eb32315772dd0b050f463308700acb2c4c0657
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-26 09:37:49 +0900

    Update angularMain.js

--- views/js/angularMain.js
+++ views/js/angularMain.js
@@ -1,5 +1,10 @@
 var app = angular.module('Main', []);
 
+app.run(function($rootScope) {
+  $rootScope.cookie_auth = document.cookie.split("%2F");
+  $rootScope.auth = $rootScope.cookie_auth[1];
+});
+
 // 화면 전환 시 login check 기능
 app.controller('logincheckCtrl', function($scope, $http, $window) {
   $scope.load = function() {
@@ -87,7 +92,7 @@ app.controller('estimateAnswerCtrl', function($scope, $window) {
 });
 
 // Estimate list 출력
-app.controller('estimateListCtrl', function($scope, $http, $window) {
+app.controller('estimateListCtrl', function($scope, $http, $window, $rootScope) {
   $scope.currentPage = 1;
   $scope.pageSize = 5;
   var cookie_auth = document.cookie.split("%2F");
@@ -101,6 +106,11 @@ app.controller('estimateListCtrl', function($scope, $http, $window) {
   var data_user;
   var data_my;
   var token_man = false;
+
+  if($rootScope.auth == 1) {
+    hideAnswer = true;
+  }
+
   // auth(사용자, 사업자)에 따른 list 변화
   if (auth == "0") { // 사용자
     $scope.HideUser = true;
@@ -273,9 +283,11 @@ app.controller('estimatedetailCtrl', function($scope, $http, $window) {
   // var cookie_user = document.cookie.substring(0, 8).split("=");
   var auth = cookie_auth[1];
   // var usercheck = cookie_user[1];
-  console.log(auth);
+  console.log(auth,"auth1");
   if(auth == 1){  //사업자
+    console.log(auth,"auth2");
     var hideAnswer = true;
+    console.log(auth,"auth1");
   }
   $http.get('/getestimatedetail', {
     params: {
