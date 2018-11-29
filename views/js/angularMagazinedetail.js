51e5ebd91e122e5f5cf4d290037f0978353cee96
nocucumberno <nocucumberno@users.noreply.github.com>
2018-11-30 04:33:44 +0900

    Update angularMagazinedetail.js

--- views/js/angularMagazinedetail.js
+++ views/js/angularMagazinedetail.js
@@ -21,12 +21,14 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
 
 app.controller('magazinedetailcard', function ($scope, $http) {
   $scope.load = function () {
+    console.log("1");
     $http.get('/likecheck').success(function (response) {
+      console.log("2");
       if (response.RESULT == "1") {
         $scope.div_login = {
           "width": "12%"
         };
-
+        console.log(scope.magazine_idx, "3");
         $http.get('/getmagazinedetail', {
           params: {
             magazine_idx: $scope.magazine_idx
@@ -34,7 +36,7 @@ app.controller('magazinedetailcard', function ($scope, $http) {
         }).success(function (response) {
           if (response.RESULT == 1) {
             console.log(response, "success");
-
+            console.log(response.INFO, "4");
             $scope.magazinedetail = response.INFO;
             /*
                     var ol = document.getElementById('ol_indicators');
