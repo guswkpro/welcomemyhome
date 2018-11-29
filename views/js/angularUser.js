bb874f1d0194da88956424f5ee92ede02c5d52f0
dnwlsrla40 <dnwlsrla40@naver.com>
2018-11-30 04:41:25 +0900

    Update angularUser.js

--- views/js/angularUser.js
+++ views/js/angularUser.js
@@ -48,27 +48,6 @@ app.controller('loginController', function ($scope, $http, $window) {
 			}
 		})
 	}
-	$scope.clickIdOverlap = function(){
-		console.log("come");
-		console.log($scope.id);
-		$http.get('/idcheck', {
-      params: {
-				user_id: $scope.id
-			}
-      }).success(function(response) {
-      console.log(response.RESULT);
-      if (response.RESULT == "0") {
-				var msg = "이미 있는 ID입니다."
-				window.alert(msg);
-      } else if (response.RESULT == "1") {
-				var msg = "ID는 비워질 수 없습니다."
-				window.alert(msg);
-      } else if(response.RESULT == "2") {
-				var msg = "사용 가능한 ID입니다."
-				window.alert(msg);
-			}
-		})
-  };
 });
 
 app.controller('signupController', function ($scope, $http, $window) {
@@ -94,5 +73,27 @@ app.controller('signupController', function ($scope, $http, $window) {
 				$window.alert(msg);
 			}
 		});
-	}
+	};
+
+	$scope.clickIdOverlap = function(){
+		console.log("come");
+		console.log($scope.id);
+		$http.get('/idcheck', {
+      params: {
+				user_id: $scope.id
+			}
+      }).success(function(response) {
+      console.log(response.RESULT);
+      if (response.RESULT == "0") {
+				var msg = "이미 있는 ID입니다."
+				window.alert(msg);
+      } else if (response.RESULT == "1") {
+				var msg = "ID는 비워질 수 없습니다."
+				window.alert(msg);
+      } else if(response.RESULT == "2") {
+				var msg = "사용 가능한 ID입니다."
+				window.alert(msg);
+			}
+		})
+  };
 });
