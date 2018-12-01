4c9f1ebcb6406484eddd38e73180d1f53b018fcb
guswkpro <guswk_pro@naver.com>
2018-12-01 19:52:08 +0900

    asd

---
+++ controller/communitydao/index.js
@@ -0,0 +1,6 @@
+var communitydao = require('../../dao/communitydao');
+var communitydto = require('../../dto/communitydto');
+var async = require('async');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+require('date-utils');
