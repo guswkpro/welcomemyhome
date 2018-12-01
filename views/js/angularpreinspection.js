85d52794a12c2f95954741287d4ff359df617227
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-02 04:04:18 +0900

    Update angularpreinspection.js

--- views/js/angularpreinspection.js
+++ views/js/angularpreinspection.js
@@ -43,20 +43,21 @@ app.controller('preinspectionCtrl', function ($scope, $http, $window) {
             dx=dy=0;
             dragging = true;
             console.log("mousedown - s:", sx, sy, "/i:", ix, iy);
-        }).on('mousemove', function(e) {
-            if(dragging) {
-                dx = e.pageX -sx;
-                dy = e.pageY -sy;
-                $(o).offset({left:ix + dx, top: iy+dy});
-                $(debug).text(dx+","+dy);
-            }
         }).on('mouseup', function(e) {
-            dx = e.pageX - sx;
+            dx = e.pageX - sx-;
             dy = e.pageY - sy;
             $(o).offset({left: ix + dx, top: iy+ dy});
             dragging = false;
             console.log("mouseup - d:", dx,dy,"/i:", ix, iy);
         });
+        $('html').on('mousemove', function(e) {
+            if(dragging) {
+                dx e.pageX -sx;
+                dy = e.pageY -sy;
+                $(o).offset({left:ix + dx, top: iy+dy});
+                $(debug).text(dx+","+dy);
+            }
+        })
     });
     // var mouseX = 0;
     // var mouseY = 0;
