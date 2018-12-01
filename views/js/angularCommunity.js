4f0d8058bdb7f1aabdc3ad9250c3ac6bbe4507a4
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-01 23:59:32 +0900

    커뮤포스트

--- views/js/angularCommunity.js
+++ views/js/angularCommunity.js
@@ -36,3 +36,60 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
       console.log(error);
     });
   });
+
+
+  app.controller('communityPostingCtrl', function($scope, $http, $window) {
+    $scope.pushCommunityData = function() {
+      var images = [];
+  
+      var recourcive = function(index) {
+        var input = document.getElementById('fileselector');
+        var fr = new FileReader();
+        fr.readAsDataURL(input.files[index]);
+        fr.onload = function() {
+          var str = fr.result.split(',')[1];
+          var image = {
+            image: str
+          };
+          images.push(image);
+          if (index == input.files.length - 1) {
+            console.log(JSON.stringify(images));
+            $http({
+              method: 'POST',
+              url: '/addestimate',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              data: ({
+                title: $scope.title,
+                address: $scope.address,
+                content: $scope.content,
+                image: images
+              })
+            }).success(function(response) {
+              if (response.RESULT == "1") {
+                var msg = "글이 등록됐습니다.";
+                $window.alert(msg);
+                $window.location.href = '/community';
+              } else {
+                var msg = "알 수 없는 오류로 글 작성에 실패하였습니다.";
+                $window.alert(msg);
+                $window.location.href='/community'
+              }
+            }).error(function() {
+              console.log("error");
+            });
+          } else {
+            recourcive(index + 1);
+          }
+        }
+      }
+      recourcive(0);
+    }
+    //estimate 작성 취소
+    $scope.cancelPosting = function() {
+      var msg = "작성을 취소하여 리스트 페이지로 이동합니다.";
+      $window.alert(msg);
+      $window.location.href = '/community';
+    };
+  });
