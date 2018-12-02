f02c290912282b1dc1a8a7db9128f579ade30472
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-02 21:29:36 +0900

    Update angularpreinspection.js

--- views/js/angularpreinspection.js
+++ views/js/angularpreinspection.js
@@ -32,7 +32,7 @@ app.controller('preinspectionCtrl', function ($scope, $http, $window) {
     // pin img 복사 이동
     $(function() {
 
-        $('.pin-img').draggable({helper: "clone", cursorAt: { top: 15, left: 15 }});
+        $('.pin-img').draggable({helper: "clone", cursorAt: { top: 0, left: 0 }});
         $('.pin-img').bind('dragstop', function(event, ui) {
             $(this).after($(ui.helper).clone().draggable());
             $(this).css({
