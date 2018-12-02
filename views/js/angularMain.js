18b2387c1001091671ec6f29cdccc4acffcd54e7
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-03 08:20:11 +0900

    update

--- views/js/angularMain.js
+++ views/js/angularMain.js
@@ -26,63 +26,6 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
   };
 });
 
-// estimate 제출 시 정보 서버로 보내는 기능
-app.controller('estimateCtrl', function($scope, $http, $window) {
-  $scope.pushEstimateData = function() {
-    var images = [];
-
-    var recourcive = function(index) {
-      var input = document.getElementById('fileselector');
-      var fr = new FileReader();
-      fr.readAsDataURL(input.files[index]);
-      fr.onload = function() {
-        var str = fr.result.split(',')[1];
-        var image = {
-          image: str
-        };
-        images.push(image);
-        if (index == input.files.length - 1) {
-          console.log(JSON.stringify(images));
-          $http({
-            method: 'POST',
-            url: '/addestimate',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            data: ({
-              title: $scope.title,
-              address: $scope.address,
-              content: $scope.content,
-              image: images
-            })
-          }).success(function(response) {
-            if (response.RESULT == "1") {
-              var msg = "견적 작성에 성공하셨습니다.";
-              $window.alert(msg);
-              $window.location.href = '/estimatelist';
-            } else {
-              var msg = "알 수 없는 오류로 견적 작성에 실패하였습니다.";
-              $window.alert(msg);
-              $window.location.href='/estimatelist'
-            }
-          }).error(function() {
-            console.log("error");
-          });
-        } else {
-          recourcive(index + 1);
-        }
-      }
-    }
-    recourcive(0);
-  }
-  //estimate 작성 취소
-  $scope.cancelEstimate = function() {
-    var msg = "작성을 취소하여 리스트 페이지로 이동합니다.";
-    $window.alert(msg);
-    $window.location.href = '/estimatelist';
-  };
-});
-
 // 사업자 답변 관련 controller
 app.controller('estimateAnswerCtrl', function($scope, $http, $window) {
   //어느 글의 답변인지 정보 가져오기
