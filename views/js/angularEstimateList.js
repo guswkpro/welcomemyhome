0c64188495827c60ce4382eb565cbd2ea1c5af35
guswkpro <guswk_pro@naver.com>
2018-12-04 05:18:16 +0900

    Update angularEstimateList.js

--- views/js/angularEstimateList.js
+++ views/js/angularEstimateList.js
@@ -1,4 +1,4 @@
-var app = angular.module('estimateList', ['ngCookies']);
+var app = angular.module('estimateList', []);
 
 // 화면 전환 시 login check 기능
 app.controller('logincheckCtrl', function ($scope, $http, $window) {
@@ -25,7 +25,7 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
 });
 
 // Estimate list 출력
-app.controller('estimateListCtrl', function ($scope, $http, $window, $cookies) {
+app.controller('estimateListCtrl', function ($scope, $http, $window) {
   $scope.currentPage = 1;
   $scope.pageSize = 5;  // var 써도 되지 않을까??
   var auth = document.cookie.split("%2F")[1];
@@ -203,7 +203,6 @@ app.controller('estimateListCtrl', function ($scope, $http, $window, $cookies) {
   // 사용자가 자기 게시글 클릭
   $scope.userClickEstimate = function (estimate_idx) {
     alert(estimate_idx);
-    console.log($cookies);
     console.log(estimate_idx);
     document.cookie.clickdata = estimate_idx;
   };
