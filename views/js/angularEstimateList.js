7228aa9ca67fa5d91dfdc27070050767d7176800
guswkpro <guswk_pro@naver.com>
2018-12-04 05:23:51 +0900

    Update angularEstimateList.js

--- views/js/angularEstimateList.js
+++ views/js/angularEstimateList.js
@@ -204,6 +204,7 @@ app.controller('estimateListCtrl', function ($scope, $http, $window) {
   $scope.userClickEstimate = function (estimate_idx) {
     alert(estimate_idx);
     console.log(estimate_idx);
+    document.cookie = "click_idx3525=asfdsfads";
     document.cookie = "click_idx=" + estimate_idx;
   };
 });
