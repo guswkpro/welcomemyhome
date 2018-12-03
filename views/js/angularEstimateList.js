eea35afabb448424f174003d06dd4d69c287fe45
guswkpro <guswk_pro@naver.com>
2018-12-04 05:07:27 +0900

    Update angularEstimateList.js

--- views/js/angularEstimateList.js
+++ views/js/angularEstimateList.js
@@ -202,7 +202,7 @@ app.controller('estimateListCtrl', function ($scope, $http, $window) {
 
   // 사용자가 자기 게시글 클릭
   $scope.userClickEstimate = function (event) {
-    alert(event.target[0]);
+    alert(event.target.child(0));
     console.log(event.target.innerHTML);
   };
 });
