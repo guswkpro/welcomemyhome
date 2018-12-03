276e736afdf9115754f431e4e4c2a1c1b02e4a26
guswkpro <guswk_pro@naver.com>
2018-12-04 04:51:04 +0900

    asd

--- views/js/angularEstimateList.js
+++ views/js/angularEstimateList.js
@@ -200,7 +200,7 @@ app.controller('estimateListCtrl', function ($scope, $http, $window) {
 
   // 사용자가 자기 게시글 클릭
   $scope.userClickEstimate = function () {
-    alert($scope.estimate_idxaa);
+    alert($scope.d.estimate_idx);
     $window.location.href = "/estimatedetail";
   };
 });
