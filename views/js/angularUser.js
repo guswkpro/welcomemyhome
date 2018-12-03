5ac76a44fbbb0bd029bf9f99ba3e6f4e7c681c25
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-04 01:30:41 +0900

    Update angularUser.js

--- views/js/angularUser.js
+++ views/js/angularUser.js
@@ -108,7 +108,7 @@ app.controller('signupController', function ($scope, $http, $window) {
 	$scope.clickNicknameOverlap = function () {
 		$http.get('/nicknamecheck', {
 			params: {
-				user_id: $scope.nickname
+				user_ninckname: $scope.nickname
 			}
 		}).success(function (response) {
 			console.log(response.RESULT);
