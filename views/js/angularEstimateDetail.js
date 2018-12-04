828bad4ded2915009e7b877b999843ae5e1b176e
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-04 16:23:08 +0900

    Update angularEstimateDetail.js

--- views/js/angularEstimateDetail.js
+++ views/js/angularEstimateDetail.js
@@ -23,44 +23,34 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
 
 //detail 컨트롤러
 app.controller('estimateDetailCtrl', function ($scope, $http, $window) {
-  //var cookie_user = document.cookie.substring(0, 8).split("=");
-  // var usercheck = cookie_user[1];
   var cookie = document.cookie.split("=");
   var click_idx = cookie[2];
-  console.log(document.cookie);
-  console.log(click_idx , "click_idx");
-  // var cookie = document.cookie.split("%2F")[1];
-  // var cookie_estimate_idx = decodeURI("84%2F0%2F_G2gJ6AEe-eHQ-XkWnQeoQpk2sgCKQXJ");
-  // console.log(cookie);
-  // console.log(cookie_estimate_idx);
 
-  // $http.get('/getestimatedetail', {
-  //   params: {
-  //     estimate_idx: "62"
-  //   }
-  // }).success(function (response) {
-  //   if (response.RESULT == 1) {
-  //     $scope.title = response.INFO.estimate_title;
-  //     $scope.date = response.INFO.estimate_date;
-  //     $scope.address = response.INFO.estimate_address;
-  //     $scope.content = response.INFO.estimate_content;
-  //     $scope.image = response.INFO.encodedimage;
-  //   } else {
-  //     var msg = "알 수 없는 에러로 detail 페이지를 불러 올 수 없습니다.";
-  //     $window.alert(msg);
-  //     $window.location.href = '/estimatelist';
-  //   }
-  // }).error(function () {
-  //   console.log("error");
-  // });
+  $http.get('/getestimatedetail', {
+    params: {
+      estimate_idx: click_idx
+    }
+  }).success(function (response) {
+    if (response.RESULT == 1) {
+      $scope.title = response.INFO.estimate_title;
+      $scope.date = response.INFO.estimate_date;
+      $scope.address = response.INFO.estimate_address;
+      $scope.content = response.INFO.estimate_content;
+      $scope.image = response.INFO.encodedimage;
+    } else {
+      var msg = "알 수 없는 에러로 detail 페이지를 불러 올 수 없습니다.";
+      $window.alert(msg);
+      $window.location.href = '/estimatelist';
+    }
+  }).error(function () {
+    console.log("error");
+  });
 });
 
 // Estimate list 출력
 app.controller('estimateListCtrl', function ($scope, $http, $window, ) {
   $scope.currentPage = 1;
   $scope.pageSize = 5;  // var 써도 되지 않을까??
-  // var cookie_user = document.cookie.substring(0, 8).split("=");
-  // var usercheck = cookie_user[1];
   var offset = 0;
   var total;
 
