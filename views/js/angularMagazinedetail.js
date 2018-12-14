8464b468bb280a5dc417c815bed3903054383101
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-15 08:36:48 +0900

    Update angularMagazinedetail.js

--- views/js/angularMagazinedetail.js
+++ views/js/angularMagazinedetail.js
@@ -66,7 +66,7 @@ app.controller('magazinedetailcard', function ($scope, $http) {
       },
       data: ({
         magazine_idx: click_idx,
-        comment: $scope.content
+        content: $scope.content
       })
     }).success(function (response) {
       if (response.RESULT == "1") {
