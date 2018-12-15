25d9fb2cd6f6ea18794bd10a6a48aa51a91a217f
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-15 23:55:20 +0900

    Update angularpreinspection.js

--- views/js/angularpreinspection.js
+++ views/js/angularpreinspection.js
@@ -159,7 +159,7 @@ app.controller('preinspectionCtrl', function ($scope, $http, $window) {
                 images.push(image);
                 if (index == input.files.length - 1) {
                     console.log(JSON.stringify(images));
-                    console.log(pin_arr[cloneCount].x, "pin-x");
+                    console.log(pin_arr[cloneCount], "pin");
                     $http({
                         method: 'POST',
                         url: '/addpreinspectionmodal',
