ab4b71c79c0c5530a505d2bb836f7a912f7ed9b1
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-16 06:10:41 +0900

    Update angularcommunitydetail.js

--- views/js/angularcommunitydetail.js
+++ views/js/angularcommunitydetail.js
@@ -49,6 +49,33 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
       console.log("error");
     });
 
+    $scope.pushCommentData = function () {
+      console.log($scope.content);
+      $http({
+        method: 'POST',
+        url: '/addcommunitycomment',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: ({
+          community_idx: click_idx,
+          content: $scope.content
+        })
+      }).success(function (response) {
+        if (response.RESULT == "1") {
+          var msg = "댓글이 등록됐습니다.";
+          $window.alert(msg);
+          $window.location.href = '/community';
+        } else if(response.RESULT == "0"){
+          var msg = "알 수 없는 오류로 댓글 작성에 실패하였습니다.";
+          $window.alert(msg);
+          $window.location.href = '/community';
+        }
+      }).error(function () {
+        console.log("error");
+      });
+    }
+
     $http.get('/getcommunitycomment', {
       params: {
         community_idx: click_idx
@@ -56,7 +83,7 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
     }).success(function (response) {
       if (response.RESULT == 1) {
         $scope.comment = response.INFO;
-        console.log(response+"겟커뮤니티댓글 반환값");
+        console.log(response.INFO+"겟커뮤니티댓글 반환값");
       } else {
         var msg = "알 수 없는 에러로 댓글을 불러올 수 없습니다.";
         $window.alert(msg);
