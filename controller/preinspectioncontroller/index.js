2c28423d8b7a9d900f2fb2da1d83d10bb81eaf28
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-15 17:38:11 +0900

    Update index.js

--- controller/preinspectioncontroller/index.js
+++ controller/preinspectioncontroller/index.js
@@ -21,7 +21,6 @@ exports.getpreinspectionblueprint = function (request, response) {
             console.log(preinspectiondata[0].preinspection_picture_path);
             preinspectiondata[0].preinspection_picture_path = preinspectiondata[0].preinspection_picture_path;
             console.log(preinspectiondata[0].preinspection_picture_path);
-            nextCallback(preinspectiondata);
             console.log("3");
         }, function (preinspectiondata, nextCallback){
             console.log("4");
