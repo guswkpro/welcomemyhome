8485abf179f9c3a855b69b0207f5fd80de1d4a9b
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-17 02:35:32 +0900

    Update angularpreinspection.js

--- views/js/angularpreinspection.js
+++ views/js/angularpreinspection.js
@@ -208,23 +208,6 @@ app.controller('preinspectionCtrl', function ($scope, $http, $window, clones) {
             cloneCount++;
             //pin 클릭시 모듈 정보 다시 띄우기
             $(".pin-img").click(function () {
-                console.log("click event")
-                $http.get('/getpreinspectionmodal', {
-                    params: {
-                        pin_idx: $(this).attr("name")
-                    }
-                }).success(function (response) {
-                    if (response.RESULT == "1") {
-                        $scope.content = response.INFO.content;
-                        $scope.type = response.INFO.type;
-                        $scope.encoded_image_modal = response.INFO.encodedimage;
-                    } else {
-                        $scope.content = '';
-                        $scope.type = '';
-                    }
-                }).error(function () {
-                    console.log("error");
-                });
                 $(".pin-img").css({
                     'display': 'block'
                 });
