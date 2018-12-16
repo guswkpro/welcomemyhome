6fb2a715828632552a0673ddf6e265aa9edd1844
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-17 06:02:46 +0900

    Update angularcommunitydetail.js

--- views/js/angularcommunitydetail.js
+++ views/js/angularcommunitydetail.js
@@ -25,6 +25,7 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
 
   app.controller('communityDetailCtrl', function ($scope, $http, $window) {
     var cookie = document.cookie.split("click_idx=");
+    console.log(document.cookie + "          aaaaaaaaaaaaaa");
     var temp_cookie = cookie[1].split("-");
     var click_idx = temp_cookie;
     let likecheck = 0;
