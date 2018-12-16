cd26e195557002921ae5715a004d375db032ef52
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-16 12:15:30 +0900

    ㅗㄷㅁㄳ

--- views/js/angularMagazinedetail.js
+++ views/js/angularMagazinedetail.js
@@ -29,24 +29,49 @@ app.controller('magazinedetailcard', function ($scope, $http, $window) {
     }
   }).success(function (response) {
     if (response.RESULT == 1) {
-      console.log(response, "success");
       $scope.magazinedetail = response.INFO
-      
-      console.log($scope.magazinedetail);
       $scope.title = response.INFO.magazine_title;
       var tmp = [];
-      for(var i = 0; i < response.INFO.encodedimage.length; i++){
+      for (var i = 0; i < response.INFO.encodedimage.length; i++) {
         tmp.push(i);
       }
       $scope.slideidx = tmp;
       document.cookie = "click_idx=";
-
     } else {
       console.log(response, "fault");
     }
   }).error(function () {
     console.log(error);
   });
+
+  $scope.pushLike = function () {
+    $http({
+      method: 'POST',
+      url: '/addmagazinelike',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: ({
+        magazine_idx: click_idx
+      })
+    }).success(function (response) {
+      if (response.RESULT == "1") {
+        $(function() {
+          $(".heart").on("click", function() {
+            $(this).toggleClass("heart-blast");
+          });
+        }); 
+      } else if (response.RESULT == "0") {
+        var msg = "요청 실패";
+        $window.alert(msg);
+      };
+    }).error(function () {
+      var msg = "로그인이 필요합니다";
+      $window.alert(msg);
+      $window.location.href = '/login';
+      console.log("error");
+    });
+  }
 
   $scope.pushCommentData = function () {
     console.log($scope.content);
@@ -65,7 +90,7 @@ app.controller('magazinedetailcard', function ($scope, $http, $window) {
         var msg = "댓글이 등록됐습니다.";
         $window.alert(msg);
         $window.location.href = '/magazines';
-      } else if(response.RESULT == "0"){
+      } else if (response.RESULT == "0") {
         var msg = "알 수 없는 오류로 댓글 작성에 실패하였습니다.";
         $window.alert(msg);
         $window.location.href = '/magazines'
@@ -80,23 +105,23 @@ app.controller('magazinedetailcard', function ($scope, $http, $window) {
       magazine_idx: click_idx
     }
   }).success(function (response) {
-    console.log(response.RESULT+"resultresult");
-      if (response.RESULT == 1) {
-        $scope.comment = response.INFO;
-        console.log(response+"000000000000000");
-        if(response.INFO.user_profile_image == null){
-          $scope.userimg = "img/user_profile_default.JPG"
-          console.log($scope.userimg+"aaaaaaaaaa");
-        }
-        else{
-          $scope.userimg = "data:image/jpeg;base64," + response.INFO.user_profile_image;
-          console.log(userimg+"bbbbbbbbbb");
-        }
-        console.log(response.INFO);
-      } else {
-        console.log(response, "fault");
+    console.log(response.RESULT + "resultresult");
+    if (response.RESULT == 1) {
+      $scope.comment = response.INFO;
+      console.log(response + "000000000000000");
+      if (response.INFO.user_profile_image == null) {
+        $scope.userimg = "img/user_profile_default.JPG"
+        console.log($scope.userimg + "aaaaaaaaaa");
       }
-    }).error(function () {
-      console.log(error);
-    });
+      else {
+        $scope.userimg = "data:image/jpeg;base64," + response.INFO.user_profile_image;
+        console.log(userimg + "bbbbbbbbbb");
+      }
+      console.log(response.INFO);
+    } else {
+      console.log(response, "fault");
+    }
+  }).error(function () {
+    console.log(error);
+  });
 });
