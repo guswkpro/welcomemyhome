3ff4369ec227b2f2dbf4f1be787a3a4d505ba98e
guswkpro <guswk_pro@naver.com>
2018-12-17 10:05:21 +0900

    Update angularMypage.js

--- views/js/angularMypage.js
+++ views/js/angularMypage.js
@@ -18,8 +18,8 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
       }
     });
   };
-  $scope.logout = function() {
-    $http.get('/logout').success(function(response) {
+  $scope.logout = function () {
+    $http.get('/logout').success(function (response) {
       console.log(response.RESULT);
       if (response.RESULT == "1") {
         var msg = "로그아웃되었습니다.";
@@ -27,7 +27,7 @@ app.controller('logincheckCtrl', function ($scope, $http, $window) {
         $window.location.href = '/';
         $scope.showHide_logout = true;
       }
-    }).error(function(){
+    }).error(function () {
       console.log(error);
     });
   };
@@ -64,68 +64,42 @@ app.controller('userCtrl', function ($scope, $http, $window) {
   $scope.pushDataUserpicture = function () {
     var input = document.getElementById('fileselector');
     var fr = new FileReader();
-      fr.readAsDataURL(input.files[0]);//input.files[0]에 사용자 로컬경로가 담기고 그게 fr에 저장
-      fr.onload = function () {
-        var str = fr.result.split(',')[1];
-        var image = str;
-        console.log(JSON.stringify(image));
-        $http({
-          method: 'POST',
-          url: '/mypagesetting',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: ({
-            image: image
-          })
-        }).success(function (response) {
-          if (response.RESULT == "1") {
-            var msg = "회원정보수정에 성공하셨습니다.";
-            $window.alert(msg);
-            $window.location.href = '/mypagesetting';
-          } else if (response.RESULT == "4") {
-            var msg = "알 수 없는 오류로 변경에 실패하였습니다.";
-            $window.alert(msg);
-            $window.location.href = '/'
-          }
-        }).error(function () {
-          console.log("error");
-        });
-      }
+    fr.readAsDataURL(input.files[0]);//input.files[0]에 사용자 로컬경로가 담기고 그게 fr에 저장
+    fr.onload = function () {
+      var str = fr.result.split(',')[1];
+      var image = str;
+      $http({
+        method: 'POST',
+        url: '/editprofile',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: ({
+          image: image
+        })
+      }).success(function (response) {
+        if (response.RESULT == "1") {
+          var msg = "회원정보수정에 성공하셨습니다.";
+          $window.alert(msg);
+          $window.location.href = '/mypagesetting';
+        } else if (response.RESULT == "0") {
+          var msg = "알 수 없는 오류로 변경에 실패하였습니다.";
+          $window.alert(msg);
+          $window.location.href = '/'
+        }
+      }).error(function () {
+        console.log("error");
+      });
+    }
   }
-
-  $scope.pushDataNickname = function () {
-        $http({
-          method: 'POST',
-          url: '/mypagesetting',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          data: ({
-            nickname: $scope.nickname
-          })
-        }).success(function (response) {
-          if (response.RESULT == "1") {
-            var msg = "회원정보수정에 성공하셨습니다.";
-            $window.alert(msg);
-            $window.location.href = '/mypagesetting';
-          } else if (response.RESULT == "4") {
-            var msg = "알 수 없는 오류로 변경에 실패하였습니다.";
-            $window.alert(msg);
-            $window.location.href = '/'
-          }
-        }).error(function () {
-          console.log("error");
-        });
-      }
 
   $scope.pushDataPassword = function () {
     $http({
       method: 'POST',
-      url: '/mypagesetting',
+      url: '/editpassword',
       headers: {
         'Content-Type': 'application/json'
-      },data: ({
+      }, data: ({
         pw: $scope.pw
       })
     }).success(function (response) {
@@ -133,7 +107,7 @@ app.controller('userCtrl', function ($scope, $http, $window) {
         var msg = "회원정보수정에 성공하셨습니다.";
         $window.alert(msg);
         $window.location.href = '/mypagesetting';
-      } else if (response.RESULT == "4") {
+      } else if (response.RESULT == "0") {
         var msg = "알 수 없는 오류로 변경에 실패하였습니다.";
         $window.alert(msg);
         $window.location.href = '/'
