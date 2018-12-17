070a65d151e5521689bac0fc1786c6feea5a6586
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-17 13:28:21 +0900

    Update angularMagazinedetail.js

--- views/js/angularMagazinedetail.js
+++ views/js/angularMagazinedetail.js
@@ -44,7 +44,7 @@ app.controller('magazinedetailcard', function ($scope, $http, $window) {
   }).success(function (response) {
     if (response.RESULT == 1) {
       $scope.magazinedetail = response.INFO;
-      console.log(scope.magazinedetail+"메거진디테일")
+      console.log($scope.magazinedetail+"메거진디테일");
       $scope.userimga = response.INFO.user_profile_image; //댓글쓰기에서 뜨는 사진
       $scope.title = response.INFO.magazine_title;
       likecheck = response.INFO.likecheck;
