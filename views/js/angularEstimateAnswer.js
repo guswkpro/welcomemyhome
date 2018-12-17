fc669be14bf473ae09ceb5107528de1d0329d558
dnwlsrla40 <dnwlsrla40@naver.com>
2018-12-17 17:27:27 +0900

    Update angularEstimateAnswer.js

--- views/js/angularEstimateAnswer.js
+++ views/js/angularEstimateAnswer.js
@@ -41,9 +41,9 @@ app.controller('logincheckCtrl', function($scope, $http, $window) {
 
 // 사업자 답변 관련 controller
 app.controller('estimateAnswerCtrl', function($scope, $http, $window) {
-  var cookie = document.cookie.substring(0,12);
-  var temp_cookie = cookie.split("=");
-  var click_idx = temp_cookie[1];
+  var cookie = document.cookie.split("click_idx=");
+  var temp_cookie = cookie[1].split("-");
+  var click_idx = temp_cookie[0];
     //어느 글의 답변인지 정보 가져오기
     // 답변 등록 기능
     $scope.pushEstimateAnswerData = function() {
